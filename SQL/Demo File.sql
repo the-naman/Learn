2d4a1a1d2bcf80ee5bf66f74ada9397f4e0875c8
the-naman <theengineernaman@gmail.com>
2026-05-23 16:18:13 +0530

    auto-sync: modified SQL\Demo File.sql

--- SQL/Demo File.sql
+++ SQL/Demo File.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS users1 (
+CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users1 (name, email) VALUES 
+INSERT INTO users (name, email) VALUES 
   ('Naman', 'naman@example.com'),
   ('Alice', 'alice@example.com'),
-  ('Bob', 'bob1@example.com');
+  ('Bob', 'bob@example.com');
 
-SELECT * FROM users1;
+SELECT * FROM users;
 
-drop table users1;
+drop table users;
