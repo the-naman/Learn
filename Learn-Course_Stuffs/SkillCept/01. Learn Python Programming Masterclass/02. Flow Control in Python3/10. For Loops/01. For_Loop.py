c82b4093f0cb87ae169d67e089c832ba37000c07
the-naman <theengineernaman@gmail.com>
2026-05-25 13:17:29 +0530

    Auto-sync: changes detected

--- Learn-Course_Stuffs/SkillCept/01. Learn Python Programming Masterclass/02. Flow Control in Python3/10. For Loops/01. For_Loop.py
+++
@@ -1,4 +0,0 @@
-animal = "Bengal Tiger"
-
-for char in animal:
-    print(char)
--- Learn-Course_Stuffs/SkillCept/01. Learn Python Programming Masterclass/02. Flow Control in Python3/10. For Loops/02. Separators_Finder_Using_FOR.py
+++
@@ -1,7 +0,0 @@
-numbers = "9, 325; 223: 372 036, 854;775"
-separators = ""
-
-for char in numbers:
-    if not char.isnumeric():
-        separators =   separators + char
-print(separators)
