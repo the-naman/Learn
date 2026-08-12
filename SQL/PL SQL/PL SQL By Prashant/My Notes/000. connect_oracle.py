46e204d4419f48f47119eca63189c881ec29f032
the-naman <theengineernaman@gmail.com>
2026-08-13 00:31:15 +0530

    Auto-sync: changes detected

--- SQL/PL SQL/PL SQL By Prashant/My Notes/000. connect_oracle.py
+++ SQL/PL SQL/PL SQL By Prashant/My Notes/000. connect_oracle.py
@@ -43,3 +43,17 @@ if shell:
         print("✨ Permanent %%plsql registered! Session is live.")
     except Exception as e:
         print(f"❌ Connection setup failed: {e}")
+
+
+# To start from any notebook
+# %run "000. connect_oracle.py"
+
+# to check connection is established correctly or not?
+'''
+%%plsql
+DECLARE
+    v_msg VARCHAR2(100) := 'Brilliant! Your database setup is officially complete.';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(v_msg);
+END;
+'''
